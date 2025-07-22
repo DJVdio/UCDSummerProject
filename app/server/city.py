0aa9a97281e9d61a341014c15e7f55156e26800e
Fangwei Hu <592161467@qq.com>
2025-07-23 00:15:05 +0100

    只返回有充电站的城市

--- app/server/city.py
+++ app/server/city.py
@@ -1,14 +1,22 @@
 from sqlalchemy import func
 from sqlalchemy.orm import Session
 
-from models import City
+from models import City, ChargingStation
 
 
-def get_all_cities(db: Session):  # 移除Depends
-    cities = db.query(
-        City.city_id,
-        City.label,
-        func.ST_X(City.center).label("lon"),
-        func.ST_Y(City.center).label("lat")
-    ).all()
+def get_all_cities(db: Session):
+    cities = (
+        db.query(
+            City.city_id,
+            City.label,
+            func.ST_X(City.center).label("lon"),
+            func.ST_Y(City.center).label("lat")
+        )
+        .filter(
+            db.query(ChargingStation)
+            .filter(ChargingStation.city_id == City.city_id)
+            .exists()
+        )
+        .all()
+    )
     return cities
